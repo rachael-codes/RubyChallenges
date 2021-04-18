5f4caba58e28d380a1315dd10ddcd4e30296d017
rachael-codes <mcbride.rac@gmail.com>
2021-04-18 10:45:15 -0400

    Add Ruby challenege problem

---
+++ secret_handshake.rb
@@ -0,0 +1,25 @@
+# Ruby Challenges (deprecated) - Medium 1 
+# Program: Secret Handshake 
+# Date: 04/18/21 
+
+# My solution 
+class SecretHandshake
+  def initialize(dec)
+    dec = dec.to_i if dec.to_i != 0 
+    dec.to_i == 0 ? @binary_array = [] : @binary_array = dec.to_s(2).chars 
+  end 
+  
+  def commands 
+    commands = []
+    commands << 'wink' if @binary_array[-1] == '1' 
+    commands << 'double blink' if @binary_array[-2] == '1'
+    commands << 'close your eyes' if @binary_array[-3] == '1' 
+    commands << 'jump' if @binary_array[-4] == '1' 
+    commands.reverse! if @binary_array[-5] == '1' 
+    commands 
+  end 
+end 
+
+
+
+
---
+++ test_secret_handshake.rb
@@ -0,0 +1,54 @@
+
+require 'minitest/autorun'
+require_relative 'secret_handshake'
+
+class SecretHandshakeTest < Minitest::Test
+  def test_handshake_1_to_wink
+    handshake = SecretHandshake.new(1) # binary - 1 
+    assert_equal ['wink'], handshake.commands
+    #wink = 1
+  end
+
+  # decimal -> program finds in binary -> interprets binary and returns an array of commands that correspond to places in the binary number 
+  def test_handshake_10_to_double_blink
+    handshake = SecretHandshake.new(2) # binary - 10 (2 places) 
+    assert_equal ['double blink'], handshake.commands
+    # 10
+  end
+
+  def test_handshake_100_to_close_your_eyes
+    handshake = SecretHandshake.new(4) # binary - 100 
+    assert_equal ['close your eyes'], handshake.commands
+  end
+
+  def test_handshake_1000_to_jump
+    handshake = SecretHandshake.new(8) # binary - 1000
+    assert_equal ['jump'], handshake.commands
+  end
+
+  def test_handshake_11_to_wink_and_double_blink
+    handshake = SecretHandshake.new(3) # binary - 11 (10 + 1) 
+    assert_equal ['wink', 'double blink'], handshake.commands
+  end
+
+  def test_handshake_10011_to_double_blink_and_wink
+    handshake = SecretHandshake.new(19) # binary - 10011 (10000 + 10 + 1)
+    assert_equal ['double blink', 'wink'], handshake.commands
+  end
+
+  def test_handshake_11111_to_double_blink_and_wink
+    handshake = SecretHandshake.new(31) # binary - 11111
+    expected = ['jump', 'close your eyes', 'double blink', 'wink']
+    assert_equal expected, handshake.commands
+  end
+
+  def test_valid_string_input
+    handshake = SecretHandshake.new('1')
+    assert_equal ['wink'], handshake.commands
+  end
+
+  def test_invalid_handshake
+    handshake = SecretHandshake.new('piggies')
+    assert_equal [], handshake.commands
+  end
+end
